ca5aeb7a15928cb5e2514c8e55e833a6b6ba4ba3
Aditya Gupta <iadityapansari@gmail.com>
2019-08-24 13:36:06 +0530

    submit budget, show balance

--- js/app.js
+++ js/app.js
@@ -16,7 +16,52 @@ class UI {
     this.itemID = 0;
   }
 
-  
+  //submit budget method
+  submitBudgetForm() {
+    const value = this.budgetInput.value;
+    if (value === '' || value < 0) {
+
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>values can not be empty or negative.</p>`;
+
+      const self = this;
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove('showItem');
+      }, 3000);
+
+    }
+    else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
+    }
+  }
+
+  //show balance
+  showBalance() {
+    const expenses = this.toatalExpenses();
+    const total = parseInt(this.budgetAmount.textContent) - expenses;
+    this.balanceAmount.textContent = total;
+    if(total < 0) {
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+    } else if(total > 0) {
+      this.balance.classList.remove('showBlack', 'showRed');
+      this.balance.classList.add('showGreen');
+    } else {
+      this.balance.classList.remove('showGreen', 'showRed');
+      this.balance.classList.add('showBlack');
+    }
+  }
+
+
+  //calcualte total expenses
+  toatalExpenses() {
+    let total = 40;  //@todo
+    return total;
+  }
+
+
 }
 
 function eventListeners() {
@@ -27,18 +72,19 @@ function eventListeners() {
   const ui = new UI();
 
   //budget form submit event
-  budgetForm.addEventListener('submit', function(event) {
+  budgetForm.addEventListener('submit', function (event) {
     event.preventDefault();
+    ui.submitBudgetForm();
 
   });
 
   //expense add event
-  expenseForm.addEventListener('submit', function() {
+  expenseForm.addEventListener('submit', function () {
     event.preventDefault();
   });
 
   //expense delete event
-  expenseList.addEventListener('click', function() {});
+  expenseList.addEventListener('click', function () { });
 
 
 }
