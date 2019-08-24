d3187e6fa9d0191433f08c24e27cd0ee8c024da5
Aditya Gupta <iadityapansari@gmail.com>
2019-08-24 14:27:03 +0530

    calculate totalExpense, add new expense

--- js/app.js
+++ js/app.js
@@ -39,13 +39,13 @@ class UI {
 
   //show balance
   showBalance() {
-    const expenses = this.toatalExpenses();
+    const expenses = this.totalExpenses();
     const total = parseInt(this.budgetAmount.textContent) - expenses;
     this.balanceAmount.textContent = total;
-    if(total < 0) {
+    if (total < 0) {
       this.balance.classList.remove('showGreen', 'showBlack');
       this.balance.classList.add('showRed');
-    } else if(total > 0) {
+    } else if (total > 0) {
       this.balance.classList.remove('showBlack', 'showRed');
       this.balance.classList.add('showGreen');
     } else {
@@ -56,11 +56,74 @@ class UI {
 
 
   //calcualte total expenses
-  toatalExpenses() {
-    let total = 40;  //@todo
+  totalExpenses() {
+    let total = this.itemList.reduce(function(init, item) {
+      return init + item.amount;
+    }, 0);
+    this.expenseAmount.textContent = total;
     return total;
   }
 
+
+  //submit expense method
+  submitExpenseForm() {
+    const expenseInput = this.expenseInput.value;
+    const amountInput = this.amountInput.value;
+    const self = this;
+
+    if (expenseInput === '' || amountInput === '' || expenseInput < 0) {
+
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>values can not be empty or negative.</p>`;
+
+      setTimeout(function () {
+        self.expenseFeedback.classList.remove('showItem');
+      }, 3000);
+
+    } else {
+      let amount = parseInt(amountInput);
+      this.expenseInput.value = '';
+      this.amountInput.value = '';
+
+      let expense = {
+        id: this.itemID,
+        title: expenseInput,
+        amount: amount
+      };
+      this.itemID++
+
+      this.itemList.push(expense);
+      this.addExpense(expense);  //adds on UI
+
+      this.showBalance();
+    }
+
+  }
+
+
+  //add expense
+  addExpense(expense) {
+
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
+
+    <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+    <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+    <div class="expense-icons list-item">
+
+     <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+      <i class="fas fa-edit"></i>
+     </a>
+     <a href="#" class="delete-icon" data-id="${expense.id}">
+      <i class="fas fa-trash"></i>
+     </a>
+    </div>
+   </div>`;
+
+    this.expenseList.appendChild(div);
+  }
 
 }
 
@@ -75,12 +138,12 @@ function eventListeners() {
   budgetForm.addEventListener('submit', function (event) {
     event.preventDefault();
     ui.submitBudgetForm();
-
   });
 
   //expense add event
   expenseForm.addEventListener('submit', function () {
     event.preventDefault();
+    ui.submitExpenseForm();
   });
 
   //expense delete event
